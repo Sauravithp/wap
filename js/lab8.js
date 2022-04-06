fa7de3efc125b8d6fbe4f9cbd9641d82ecb36126
Sauravi Thapa <sauravithapa@Sauravis-MacBook-Pro.local>
2022-04-05 22:24:59 -0500

    complete lab 8 1 2 4

---
+++ js/lab8.js
@@ -0,0 +1,160 @@
+/*
+Question No 1. Object literals and inheritance
+ */
+
+let student = {
+    firstName: '',
+    lastName: '',
+    grades: [],
+    inputNewGrades: function (newGrades) {
+        this.grades.push(newGrades)
+    },
+    computeAverageGrades: function () {
+        return this.grades.reduce(function (accumulator, current) {
+            return accumulator + current;
+        }) / this.grades.length;
+    }
+}
+
+let student1 = Object.create(student);
+student1.firstName = 'Sauravi';
+student1.lastName = 'thapa';
+student1.grades = [];
+student1.inputNewGrades(1);
+student1.inputNewGrades(2);
+
+
+let student2 = Object.create(student);
+student2.firstName = 'Manju';
+student2.lastName = 'Thapa';
+student2.grades = [];
+student2.inputNewGrades(3);
+student2.inputNewGrades(4);
+
+let student3 = Object.create(student);
+student3.firstName = 'Rabin';
+student3.lastName = 'Thapa';
+student3.grades = [];
+student3.inputNewGrades(5);
+student3.inputNewGrades(6);
+
+let students = [student1, student2, student3];
+
+let total = 0;
+for (let i = 0; i < students.length; i++) {
+    total = total + students[i].computeAverageGrades();
+}
+
+console.log(total / students.length);
+
+console.log(student1)
+
+
+/*
+Question no 2  Redo question no 1 using Constructor
+ */
+
+function Student(firstName, lastName) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.grades = [];
+}
+
+Student.prototype.inputNewGrades = function (grades) {
+    this.grades.push(grades);
+}
+
+Student.prototype.computeAverageGrades = function () {
+    return this.grades.reduce(function (accumulator, current) {
+        return accumulator + current;
+    }) / this.grades.length;
+}
+
+let student4 = new Student("Suman", "Acharya");
+student4.inputNewGrades(5)
+student4.inputNewGrades(6)
+
+let student5 = new Student("Mandevi", "Budathoki");
+student5.inputNewGrades(3)
+student5.inputNewGrades(4)
+
+let student6 = new Student("Aishwarya", "Dhakal");
+student6.inputNewGrades(1)
+student6.inputNewGrades(2)
+
+
+let stdArray = [student4, student5, student6];
+
+let total1 = 0;
+for (let i = 0; i < stdArray.length; i++) {
+    total1 = total1 + stdArray[i].computeAverageGrades();
+}
+
+console.log(total1 / students.length);
+
+
+/*
+Questio No 3
+ */
+
+/*
+Question no 4 with constructor
+ */
+
+function SingleyList(){
+    this.list=[];
+}
+
+SingleyList.prototype.add=function (number){
+    this.list.push(number);
+}
+
+SingleyList.prototype.remove=function (number){
+    let test=[];
+    for (let i=0;i<this.list.length;i++){
+        if(this.list[i]!=number){
+            test.push(this.list[i]);
+        }
+    }
+    this.list=test;
+}
+
+let singleyList1=new SingleyList();
+singleyList1.add(1);
+singleyList1.add(2);
+singleyList1.add(3);
+
+console.log(singleyList1.list.forEach(s=>console.log(s)))
+singleyList1.remove(2);
+console.log(singleyList1.list.forEach(s=>console.log(s)))
+
+/*
+Question no 4 with object literals
+ */
+
+console.log("Question no 4 with object literals </br>");
+
+let linkedList={
+     list:[],
+    add: function (number){
+        this.list.push(number);
+    },
+    remove:function (number){
+        let test=[];
+        for (let i=0;i<this.list.length;i++){
+            if(this.list[i]!=number){
+                test.push(this.list[i]);
+            }
+        }
+        this.list=test;
+    }
+}
+
+let linkedlist=Object.create(linkedList);
+linkedlist.list=[];
+linkedlist.add(1);
+linkedlist.add(2);
+linkedlist.add(3);
+console.log(linkedlist.list.forEach(s=>console.log(s)))
+linkedlist.remove(2);
+console.log(linkedlist.list.forEach(s=>console.log(s)))
--- js/test.js
+++ js/test.js
@@ -1,12 +1,1 @@
-const user = {
-    salute: "",
-    greet: function() {
-        this.salute = "Hello";
-        console.log(this.salute); //Hello
-        const setFrench = function(newSalute) { //inner function
-            this.salute = newSalute; };
-        setFrench("Bonjour");
-        console.log(this.salute); //Bonjour??
-    }
-};
-user.greet(); //Hello  Hello  ??
+//
